728d9298ff82948baeaa9bc2cb5b4b7e7010330d
ralph5566 <116739066+ralph5566@users.noreply.github.com>
2023-03-06 01:19:54 +0900

    23/3/5 JV observer switch

--- js/main.js
+++ js/main.js
@@ -5,19 +5,84 @@ const sectionOne = document.getElementById("model_s");
 const sections = document.querySelectorAll(".home");
 // console.log(sections);
 
-options = { root: null, treshold: 0, rootMargin: "0px" };
+const title = document.getElementsByClassName("car_model_title")[0];
+const paragraph = document.getElementsByTagName("p")[0];
+const link = document.getElementsByClassName("car_model_link")[0];
+const left_btn = document.getElementsByClassName("home_button_order")[0];
+const right_btn = document.getElementsByClassName("home_button_exist")[0];
+// console.log(title);
 
-const observer = new IntersectionObserver(function (entries) {
-  // console.log(entries[0].target);
-  //   console.log(entries);
-  entries.forEach((entry) => {
-    console.log(entry.target);
-    console.log(entry.isIntersecting);
+let page = new Page(sections, title, paragraph, link, left_btn, right_btn);
+creatObserver(sections);
+
+function creatObserver(ele) {
+  let options = {
+    root: null,
+    threshold: 0.5,
+    rootMargin: "0px",
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  // observer.observe(sectionOne);
+
+  sections.forEach((ele) => {
+    observer.observe(ele);
   });
-}, options);
+}
 
-// observer.observe(sectionOne);
+function handleIntersect(entries, observer) {
+  // console.log(entries);
+  entries.forEach((entry) => {
+    // console.log(entry);
+    if (entry.isIntersecting) {
+      let current_id = entry.target.id;
+      // console.log(entry.target);
+      // console.log(entry.target.id);
 
-sections.forEach((section) => {
-  observer.observe(section);
-});
+      switch (current_id) {
+        case "model_s":
+          page.title = "Model S";
+          page.paragraph = "Order Online for ";
+          page.link = "Touchless Delivery";
+          page.left_btn = "Custom Order";
+          page.right_btn = "Existing Inventory";
+          break;
+        case "model_y":
+          page.title = "Model Y";
+          break;
+        case "model_3":
+          page.title = "Model 3";
+          break;
+        case "model_x":
+          page.title = "Model X";
+          page.paragraph = "Order Online for ";
+          page.link = "Touchless Delivery";
+          page.left_btn = "Custom Order";
+          page.right_btn = "Existing Inventory";
+          break;
+        case "solar_panels":
+          page.title = "Solar Panels";
+          page.paragraph = "Lowest Cost Solar Panels in American";
+          page.link = "";
+          page.left_btn = "Order Now";
+          page.right_btn = "Learn More";
+          break;
+        case "solar_roof":
+          page.title = "Solar Roof";
+          break;
+        case value:
+
+        default:
+          break;
+      }
+
+      title.innerHTML = page.title;
+      paragraph.childNodes[0].textContent = page.paragraph;
+      link.childNodes[0].textContent = page.link;
+      // console.log(left_btn.childNodes);
+      left_btn.childNodes[0].textContent = page.left_btn;
+      right_btn.childNodes[0].textContent = page.right_btn;
+    }
+  });
+}
---
+++ js/page.js
@@ -0,0 +1,10 @@
+class Page {
+  constructor(_sections, _title, _paragraph, _link, _left_btn, _right_btn) {
+    this.sections = _sections;
+    this.title = _title;
+    this.paragraph = _paragraph;
+    this.link = _link;
+    this.left_btn = _left_btn;
+    this.right_btn = _right_btn;
+  }
+}
